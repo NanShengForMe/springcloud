c4a2ac6fd3478eb37d567b4b882fa88aeb51e992
zoujidi <1045968404@qq.com>
2020-08-03 16:26:38 +0800

    Hystrix之服务熔断案例

--- cloud-provider-hystrix-payment8001/src/main/java/cn/zoujidi/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/zoujidi/springcloud/service/PaymentService.java
@@ -1,9 +1,12 @@
 package cn.zoujidi.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +18,8 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class PaymentService {
+
+    // ===============================服务降级======================================
 
     /**
      * Description: 正常访问，肯定OK
@@ -50,6 +55,32 @@ public class PaymentService {
 
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "线程池：" + Thread.currentThread().getName() + "    8001系统繁忙或运行报错，请稍后再试,id:    " + id + "\t" + "┭┮﹏┭┮";
+    }
+
+    // =============================服务熔断=======================================
+
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            // 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            // 请求次数
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            // 失败率达到多少后跳闸
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("========id不能为负数");
+        }
+        // 等价于UUID.randomUUID().toString()，并且已经去掉下划线
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName() + "\t" + "调用成功，流水号：" + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id 不能为负数，请稍后再试，┭┮﹏┭┮ id：" + id;
     }
 
 }
