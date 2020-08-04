eca49acc75d3f19a80f418b3751c445184013f9a
zoujidi <1045968404@qq.com>
2020-08-04 15:36:26 +0800

    Gateway9527搭建

---
+++ cloud-gateway-gateway9527/src/main/java/cn/zoujidi/springcloud/GatewayMain9527.java
@@ -0,0 +1,21 @@
+package cn.zoujidi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 启动类
+ *
+ * @author zoujidi
+ * @version 1.0.0
+ * @date 2020年08月04日 15:03:00
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
