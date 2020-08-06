f670028121442ddefe92f8c304ea1b8379e36547
zoujidi <1045968404@qq.com>
2020-08-06 16:59:03 +0800

    Stream消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/cn/zoujidi/springcloud/StreamMQMain8802.java
@@ -0,0 +1,21 @@
+package cn.zoujidi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 主启动类
+ *
+ * @author zoujidi
+ * @version 1.0.0
+ * @date 2020年08月06日 16:43:00
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
